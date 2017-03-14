2d8ed51adf7667122edf8ea31ca30342420162c3
Azonic89 <ooze89@yahoo.com>
2017-03-14 05:33:35 +0200

    Data layer + refactoring

---
+++ CarSystem/Data/CarSystem.Data/CarSystemDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarSystem.Data.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace CarSystem.Data
+{
+    public class CarSystemDbContext : IdentityDbContext<User>
+    {
+        public CarSystemDbContext()
+            : base("CarSystemDb")
+        {
+        }
+
+        public static CarSystemDbContext Create()
+        {
+            return new CarSystemDbContext();
+        }
+    }
+}
