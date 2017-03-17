94286b851a1192c727d070b8951d872a9fa92dfc
Azonic89 <ooze89@yahoo.com>
2017-03-17 21:08:09 +0200

    Fixes

--- CarSystem/Data/CarSystem.Data.Models/AdvertImage.cs
+++ CarSystem/Data/CarSystem.Data.Models/AdvertImage.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CarSystem.Data.Models
 {
-    class AdvertImage
+    public class AdvertImage
     {
+        [Key]
+        public int Id { get; set; }
+
+        public byte[] ImageData { get; set; }
+
+        public string ImageName { get; set; }
+
+        public int AdvertImageId { get; set; }
+
+        [ForeignKey("AdvertImageId")]
+        public virtual AdvertImage Image { get; set; }
     }
 }
