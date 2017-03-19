2846d5874a6fa92fb266384bf4acc18ac51e65da
Azonic89 <ooze89@yahoo.com>
2017-03-19 22:03:53 +0200

    Added some Data Tests

---
+++ CarSystem/Tests/CarSystem.Data.Tests/CarSystemDbContext.Tests/ConstructorShould.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+using CarSystem.Data.Contracts;
+
+namespace CarSystem.Data.Tests.CarSystemDbContext.Tests
+{
+    [TestFixture]
+    public class ConstructorShould
+    {
+        [Test]
+        public void Constructor_Should_HaveParameterlessConstructor()
+        {
+            // Arrange & Act
+            var context = new Data.CarSystemDbContext();
+
+            // Assert
+            Assert.IsInstanceOf<Data.CarSystemDbContext>(context);
+        }
+
+        [Test]
+        public void Constructor_Should_Return_InstanceOfIPetsWonderlandDbContext()
+        {
+            // Arrange & Act
+            var context = new Data.CarSystemDbContext();
+
+            // Assert
+            Assert.IsInstanceOf<ICarSystemDbContext>(context);
+        }
+    }
+}
---
+++ CarSystem/Tests/CarSystem.Data.Tests/CarSystemDbContext.Tests/CreateShould.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+using CarSystem.Data.Contracts;
+
+namespace CarSystem.Data.Tests.CarSystemDbContext.Tests
+{
+    [TestFixture]
+    public class CreateShould
+    {
+        [Test]
+        public void Create_Should_ReturnNewDbContextInstance()
+        {
+            // Arrange & Act
+            var newContext = Data.CarSystemDbContext.Create();
+
+            // Assert
+            Assert.IsNotNull(newContext);
+            Assert.IsInstanceOf<ICarSystemDbContext>(newContext);
+        }
+    }
+}
