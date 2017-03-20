b17e89de8d607c586184b9b4b9391a97124384f6
Azonic89 <ooze89@yahoo.com>
2017-03-20 03:10:34 +0200

    Finished UpdateAdvert and GetAllAdvertsByUserId unit test in Advert Service

---
+++ CarSystem/Tests/CarSystem.Data.Services.Tests/AdvertServiceTests/GetAllAdvertsByUserIdShould.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using NUnit.Framework;
+using Moq;
+
+using CarSystem.Data.Contracts;
+using CarSystem.Data.Models;
+
+namespace CarSystem.Data.Services.Tests.AdvertServiceTests
+{
+    [TestFixture]
+    public class GetAllAdvertsByUserIdShould
+    {
+        [Test]
+        public void GetAllAdvertsByUserIdShould_ShouldReturn_CorrectAdvertsAccordingToUserIdAsQueryable()
+        {
+            // Arrange
+            string userId = "asdasdasd";
+            var mockedDataProvider = new Mock<IEfCarSystemDataProvider<Advert>>();
+            var advertService = new AdvertService(mockedDataProvider.Object);
+
+            mockedDataProvider.Setup(rep => rep.All()).Returns(() => new List<Advert>() {
+                new Advert { Id = 1, UserId = userId },
+                new Advert { Id = 2, UserId = userId },
+                new Advert { Id = 3, UserId = userId },
+                new Advert { Id = 4, UserId = userId }
+            }.AsQueryable());
+
+            // Act
+            var result = advertService.GetAllAdvertsByUserId(userId).ToList();
+
+            // Assert
+            Assert.AreEqual(4, result.Count);
+        }
+    }
+}
---
+++ CarSystem/Tests/CarSystem.Data.Services.Tests/AdvertServiceTests/UpdateShould.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+using NUnit.Framework;
+using Moq;
+
+using CarSystem.Data.Contracts;
+using CarSystem.Data.Models;
+
+namespace CarSystem.Data.Services.Tests.AdvertServiceTests
+{
+    [TestFixture]
+    public class UpdateShould
+    {
+        [Test]
+        public void UpdateAdvert_Should_BeCalled_IfAdvertIsValid()
+        {
+            // Arrange
+            var mockedDataProvider = new Mock<IEfCarSystemDataProvider<Advert>>();
+            var advertService = new AdvertService(mockedDataProvider.Object);
+            var advertToUpdate = new Mock<Advert>();
+
+            // Act
+            advertService.UpdateAdvert(advertToUpdate.Object);
+
+            // Assert
+            mockedDataProvider.Verify(rep => rep.Update(advertToUpdate.Object), Times.Once);
+        }
+
+        [Test]
+        public void UpdateAdvert_ShouldThrowNullReferenceException_IfAdvertIsNull()
+        {
+            // Arrange & Act
+            var mockedDataProvider = new Mock<IEfCarSystemDataProvider<Advert>>();
+            var advertService = new AdvertService(mockedDataProvider.Object);
+            Mock<Advert> advertAsNull = null;
+
+            // Assert
+            Assert.Throws<NullReferenceException>(() => advertService.UpdateAdvert(advertAsNull.Object));
+        }
+
+        [Test]
+        public void UpdateAdvert_Should_NotUpdateAdvertThatIsNeverUpdatedInTheFirstPlace()
+        {
+            // Arrange & Act
+            var mockedDataProvider = new Mock<IEfCarSystemDataProvider<Advert>>();
+            var advertService = new AdvertService(mockedDataProvider.Object);
+            var advertThatIsNotAdded = new Mock<Advert>();
+
+            // Assert
+            mockedDataProvider.Verify(rep => rep.Update(advertThatIsNotAdded.Object), Times.Never);
+        }
+
+        [Test]
+        public void UpdateAdvert_Should_CallSaveChangesTwoTimes_IfAdvertIsValid()
+        {
+            // Arrange & Act
+            var mockedDataProvider = new Mock<IEfCarSystemDataProvider<Advert>>();
+            var advertService = new AdvertService(mockedDataProvider.Object);
+            var advert = new Mock<Advert>();
+            var secondAdvert = new Mock<Advert>();
+
+            // Act
+            advertService.UpdateAdvert(advert.Object);
+            advertService.UpdateAdvert(secondAdvert.Object);
+
+            // Assert
+            mockedDataProvider.Verify(u => u.SaveChanges(), Times.Exactly(2));
+        }
+
+        [Test]
+        public void UpdateAdvert_Should_NotCallSaveChanges_IfAdvertIsNotDelete()
+        {
+            // Arrange & Act
+            var mockedDataProvider = new Mock<IEfCarSystemDataProvider<Advert>>();
+            var advertService = new AdvertService(mockedDataProvider.Object);
+            var advertThatIsNotAdded = new Mock<Advert>();
+
+            // Assert
+            mockedDataProvider.Verify(u => u.SaveChanges(), Times.Never);
+        }
+    }
+}
