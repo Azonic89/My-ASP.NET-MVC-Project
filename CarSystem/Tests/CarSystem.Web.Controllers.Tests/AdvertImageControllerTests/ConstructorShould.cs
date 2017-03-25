1c0d56f20a94552010e61f3fb93a93658e59e74e
Azonic89 <ooze89@yahoo.com>
2017-03-25 17:57:04 +0200

    More Controller Tests

---
+++ CarSystem/Tests/CarSystem.Web.Controllers.Tests/AdvertImageControllerTests/ConstructorShould.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using NUnit.Framework;
+using Moq;
+
+using CarSystem.Data.Services.Contracts;
+
+namespace CarSystem.Web.Controllers.Tests.AdvertImageControllerTests
+{
+    [TestFixture]
+    public class ConstructorShould
+    {
+        [Test]
+        public void Constructor_Should_ThrowArgumentNullException_WhenAdvertImageServiceParameterIsNull()
+        {
+            // Arrange
+            IAdvertImageService nullAdvertImageService = null;
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new AdvertImageController(nullAdvertImageService));
+        }
+
+        [Test]
+        public void Constructor_Should_CreateInstanceOfAdvertImageService_WhenAdvertImageServiceParameterIsNotNull()
+        {
+            // Arrange
+            var mockdedAdvertImageService = new Mock<IAdvertImageService>();
+            var advertImageController = new AdvertImageController(mockdedAdvertImageService.Object);
+
+            // Act & Assert
+            Assert.That(advertImageController, Is.Not.Null);
+            Assert.IsInstanceOf<AdvertImageController>(advertImageController);
+        }
+    }
+}
---
+++ CarSystem/Tests/CarSystem.Web.Controllers.Tests/AdvertImageControllerTests/IndexShould.cs
@@ -0,0 +1,48 @@
+﻿using Moq;
+using TestStack.FluentMVCTesting;
+using NUnit.Framework;
+
+using CarSystem.Data.Models;
+using CarSystem.Data.Services.Contracts;
+
+namespace CarSystem.Web.Controllers.Tests.AdvertImageControllerTests
+{
+    [TestFixture]
+    public class IndexShould
+    {
+        [Test]
+        public void Index_Should_ReturnCorrectImageParams()
+        {
+            // Arrange
+            var advertImage = new AdvertImage
+            {
+                Id = 1,
+                ImageData = new byte[5],
+                ImageName = "ContentType"
+            };
+
+            var mockedAdvertImageService = new Mock<IAdvertImageService>();
+            mockedAdvertImageService.Setup(x => x.GetById(It.IsAny<int>())).Returns(advertImage);
+
+            var advertImageController = new AdvertImageController(mockedAdvertImageService.Object);
+
+            // Act & assert
+            advertImageController.WithCallTo(x => x.Index(It.IsAny<int>())).ShouldRenderFileContents(advertImage.ImageData, advertImage.ImageName);
+        }
+
+        [Test]
+        public void Index_Should_ReturnNullImageParams()
+        {
+            // Arrange
+            AdvertImage nullAdvertImage = null;
+            var mockedAdvertImageService = new Mock<IAdvertImageService>();
+
+            mockedAdvertImageService.Setup(x => x.GetById(It.IsAny<int?>())).Returns(nullAdvertImage);
+
+            var advertImageController = new AdvertImageController(mockedAdvertImageService.Object);
+
+            // Act & assert
+            advertImageController.WithCallTo(x => x.Index(It.IsAny<int>())).ShouldReturnContent(null);
+        }
+    }
+}
